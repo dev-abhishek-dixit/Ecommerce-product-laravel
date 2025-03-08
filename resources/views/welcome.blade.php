b87314392d3f2e2a89f6674bdb5001320011eadd
abhishek200319 <111955240+abhishek200319@users.noreply.github.com>
2025-03-08 19:45:49 +0530

    testing part2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -91,7 +91,7 @@
                                 <h2 class="mt-6 text-xl font-semibold text-gray-900 dark:text-white">Laravel News Testing</h2>
 
                                 <p class="mt-4 text-gray-500 dark:text-gray-400 text-sm leading-relaxed">
-                                    Testing.
+                                    Testing part 2.
                                 </p>
                             </div>
 
